cb6e9dd87cbe65d1902674a6499ce966d5f880e3
Pame Plúas <pamepluas@gmail.com>
2018-09-08 14:13:24 -0500

    000 [Pame] Add fetch all to http://worldcup.sfg.io/matches and add logic to get number of countries

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,12 +1,51 @@
 import React, {Component} from 'react';
 import './App.css';
 import Card from "./Card";
+import App from "./App";
 
 class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {data: []}
+    }
+
+    getWorldCupData() {
+        return fetch('http://worldcup.sfg.io/matches', {
+            headers: new Headers({
+                Accept: 'application/json',
+            }),
+            method: 'GET',
+            credentials: 'same-origin'
+        }).then(response => response.json())
+            .catch(err => err);
+    }
+
+    componentDidMount() {
+        this.getWorldCupData()
+            .then((data) => {
+                this.setState({data})
+            });
+    }
+
+    getNumberOfCountries() {
+        const matches = this.state.data;
+        const countries = [];
+        return matches
+            .map(match => match.home_team_country)
+            .filter(country => {
+                if (countries.includes(country)) {
+                    return false;
+                }
+                countries.push(country);
+                return true;
+            })
+            .reduce((acc => ++acc), 0);
+    }
+
     render() {
         return (
             <div className="row">
-                <Card/>
+                <Card title="Number of countries" value={this.getNumberOfCountries()}/>
                 <Card/>
                 <Card/>
             </div>
